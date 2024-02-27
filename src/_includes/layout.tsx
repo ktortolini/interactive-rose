3b479e9afd2b6baa3cd9065594ec3f6af4f3c2f1
ktortolini <134340991+ktortolini@users.noreply.github.com>
2024-02-26 23:13:04 -0600

    Update layout tsx file in /_includes/ for improved readability.

--- src/_includes/layout.tsx
+++ src/_includes/layout.tsx
@@ -1,20 +1,23 @@
 export default ({ title, children }: Lume.Data, helpers: Lume.Helpers) => {
 	return (
-		<html data-theme="dark">
+		<html data-theme = 'dark'>
 			<head>
-				<meta charSet='utf-8' />
+				<meta charSet = 'utf-8' />
 				<meta
-					name='viewport'
-					content='width=device-width, initial-scale=1'
+					name = 'viewport'
+					content = 'width=device-width, initial-scale=1'
 				/>
 				<title>{title}</title>
-				<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css" />
-				<link rel="stylesheet" href="/styles/fade.css" />
-				<script src="https://cdn.jsdelivr.net/npm/p5@1.9.0/lib/p5.js" />
-				<script defer src='/static/interactive-rose.js' />
+				<link
+					rel = 'stylesheet'
+					href = 'https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css'
+				/>
+				<link rel = 'stylesheet' href = '/styles/fade.css' />
+				<script src = 'https://cdn.jsdelivr.net/npm/p5@1.9.0/lib/p5.js' />
+				<script src = '/static/interactive-rose.js' defer />
 			</head>
 			<body>
-				<main className="container">{children}</main>
+				<main className = 'container'>{children}</main>
 			</body>
 		</html>
 	);
