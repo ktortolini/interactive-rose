063a48a1ab754b29d12f0cffc8857c609fb2159d
ktortolini <134340991+ktortolini@users.noreply.github.com>
2024-02-26 23:12:10 -0600

    Update the rose tsx file in /pages/ for improved readability.

--- src/pages/rose.tsx
+++ src/pages/rose.tsx
@@ -2,13 +2,15 @@ export const title = 'Interactive Rose';
 
 export const layout = '../_includes/layout.tsx';
 
+const customStyle = {
+	display: 'flex',
+	justifyContent: 'center',
+};
+
 export default (data: Lume.Data, helpers: Lume.Helpers) => {
 	return (
 		<>
-			<div
-				id='interactive-rose'
-				style='display: flex; justify-content: center;'
-			>
+			<div id = 'interactive-rose' style = {customStyle}>
 			</div>
 		</>
 	);
